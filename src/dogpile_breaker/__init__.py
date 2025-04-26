ed8266398af3244936d06aca929b8596cd7541d1
Dmitry P. <wwarne@gmail.com>
2025-04-27 01:10:04 +0300

    Bump version

--- src/dogpile_breaker/__init__.py
+++ src/dogpile_breaker/__init__.py
@@ -1,12 +1,20 @@
-__version__ = "0.9.0"
+__version__ = "0.10.1"
 
 from .api import CacheRegion, ShouldCacheFunc, StorageBackend
+from .backends.memory_backend import MemoryBackendLRU
 from .backends.redis_backend import RedisSentinelBackend, RedisStorageBackend
+from .middlewares.base_middleware import StorageBackendMiddleware
+from .middlewares.circut_breaker_fallback_middleware import CircuitBreakerFallbackMiddleware
+from .middlewares.prometheus_middleware import PrometheusMiddleware
 
 __all__ = [
     "CacheRegion",
+    "CircuitBreakerFallbackMiddleware",
+    "MemoryBackendLRU",
+    "PrometheusMiddleware",
     "RedisSentinelBackend",
     "RedisStorageBackend",
     "ShouldCacheFunc",
     "StorageBackend",
+    "StorageBackendMiddleware",
 ]
